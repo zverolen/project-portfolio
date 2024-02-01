89da4a94ee7d428f2646a412db82f561dc5e1522
Lena Zvereva <zverolen@gmail.com>
2024-02-01 14:26:45 +0100

    Add image files

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,22 @@ export default function About() {
           <h2 className="site-frame">{about.heading}</h2>
         </div>
       <div className="site-frame">
-        <img src="./../../images/portrait.png"/>
+        <img 
+          src="./../../images/portrait-100.jpg"
+          sizes="(min-width: 100px) 200px,
+                 (min-width: 501px) 20vw,
+                 100px"
+          srcSet="./../../images/portrait-100.jpg 100w,
+                  ./../../images/portrait-160.jpg 160w,
+                  ./../../images/portrait-240.jpg 240w,
+                  ./../../images/portrait-400.jpg 400w,
+                  ./../../images/portrait-600.jpg 600w"
+                  // srcSet="./../../images/portrait-100.jpg 100w,
+                  // ./../../images/portrait-160.jpg 160w,
+                  // ./../../images/portrait-240.jpg 240w,
+                  // ./../../images/portrait-600.jpg 600w"
+          alt="My smiling photo"
+        />
         <p>{about.p1}</p>
         <p>{about.p2}</p>
         <p>{about.p3}</p>
