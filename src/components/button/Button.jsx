8c50d5dbe40b295b57308ddeeeb8ea037768e331
Lena Zvereva <zverolen@gmail.com>
2024-02-07 12:12:13 +0100

    Update README and make corresponding changes in code

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 export default function Button({ isExpanded, handleClick }) {
   return (
     <button onClick={handleClick} aria-expanded={isExpanded} >
-      {isExpanded ? 'Close' : 'View'}
+      {isExpanded ? 'Hide' : 'Show'}
     </button>
   )
 }
