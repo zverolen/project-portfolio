1229c4022ed56ee099fcaa71319bfa249d502c2b
Lena Zvereva <zverolen@gmail.com>
2024-03-22 10:14:09 +0100

    Add react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements
+} from "react-router-dom"
+
+const router = createBrowserRouter( createRoutesFromElements(
+  <Route path="/" element={ <App/> }>
+    {/* <Route path="o2" element={ <O2Container />} /> */}
+    {/* <Route path="night-flight" element={ <NightFlightContainer />} /> */}
+    {/* <Route path="znaju" element={ <ZnajuContainer />} /> */}
+  </Route>
+))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
